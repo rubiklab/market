71f236788341447030acd3823c064a61ffe65c16
morteza <emortezarajabi@gmail.com>
2024-01-31 22:48:31 +1100

    fix post_deploy_scripts.sh

--- .platform/hooks/postdeploy/post_deploy_scripts.sh
+++ .platform/hooks/postdeploy/post_deploy_scripts.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#ref: https://aschmelyun.com/blog/fixing-permissions-issues-with-docker-compose-and-php/
-export USERID=$(id -u ec2-user) && export GROUPID=$(id -g ec2-user)
+chown -R USERID:GROUPID ./
--- .platform/hooks/prebuild/set_env_variables.sh
+++ .platform/hooks/prebuild/set_env_variables.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-chown -R USERID:GROUPID ./
+#ref: https://aschmelyun.com/blog/fixing-permissions-issues-with-docker-compose-and-php/
+export USERID=$(id -u ec2-user) && export GROUPID=$(id -g ec2-user)
+
