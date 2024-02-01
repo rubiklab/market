705ac1231861b6c7f5fcf011a7dfe8edcc717add
morteza <emortezarajabi@gmail.com>
2024-02-01 18:30:40 +1100

    fix post deploy scripts

--- .platform/hooks/postdeploy/post_deploy_scripts.sh
+++ .platform/hooks/postdeploy/post_deploy_scripts.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-#docker-compose run php chown -R "$USERID:$GROUPID" /var/www/html/
-chown -R ec2-user:ec2-user ./
+
+#ref: https://aschmelyun.com/blog/fixing-permissions-issues-with-docker-compose-and-php/
+export USERID=$(id -u ec2-user)
+export GROUPID=$(id -g ec2-user)
+
+docker-compose down
+docker-compose up -d --build
+
+chown -R "$USERID:$GROUPID" ./
+
 docker-compose run assets npm install
 docker-compose run assets npm run build
 docker-compose run php php artisan storage:link
