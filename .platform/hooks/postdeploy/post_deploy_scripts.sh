bc9a49198212c93220b6d23f93bd12372a9fd6f1
morteza <emortezarajabi@gmail.com>
2024-02-01 17:53:57 +1100

    fix file permission issue

--- .platform/hooks/postdeploy/post_deploy_scripts.sh
+++ .platform/hooks/postdeploy/post_deploy_scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker-compose run php chown -R "$USERID:$GROUPID" /var/www/html/
-
+#docker-compose run php chown -R "$USERID:$GROUPID" /var/www/html/
+chown -R ec2-user:ec2-user ./
 docker-compose run assets npm install
 docker-compose run assets npm run build
 docker-compose run php php artisan storage:link
