faeefc262dcf06e36a7ed47fb326615ab59fef09
morteza <emortezarajabi@gmail.com>
2024-02-04 20:46:00 +1100

    fix https issue for assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
-        //
+        if(env('APP_ENV') !== 'local') {
+            $url->forceSchema('https');
+        }
     }
 }
